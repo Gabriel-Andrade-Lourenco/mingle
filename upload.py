5738d9ffd9e91a331acd8dc70b1a7345935abf99
Gabriel-Andrade-Lourenco <gabriel.lourenco2107@gmail.com>
2025-02-14 13:57:09 -0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -16,20 +16,20 @@ selected_archives = []
 if PAGE_SELECTION_KEY not in st.session_state:
     st.session_state[PAGE_SELECTION_KEY] = "Atualize Empresa"
 
-# Comenta os botões da barra lateral para "Adicione Empresa"
+# Comenta os botões da barra lateral
 # create_company_page = st.sidebar.button("Adicione Empresa")
 update_company_page = st.sidebar.button("Atualize Empresa")
-company_page = st.sidebar.button("Empresas")
-chat_page = st.sidebar.button("Chat")
+# company_page = st.sidebar.button("Empresas")
+# chat_page = st.sidebar.button("Chat")
 
-# Mantém a lógica para mudar a página, mas comenta a parte de "Adicione Empresa"
+# Comenta a lógica de seleção de página, exceto para "Atualize Empresa"
 # if create_company_page:
 #     st.session_state[PAGE_SELECTION_KEY] = "Adicione Empresa"
-if company_page:
-    st.session_state[PAGE_SELECTION_KEY] = "Empresas"
-elif chat_page:
-    st.session_state[PAGE_SELECTION_KEY] = "Chat"
-elif update_company_page:
+# if company_page:
+#     st.session_state[PAGE_SELECTION_KEY] = "Empresas"
+# elif chat_page:
+#     st.session_state[PAGE_SELECTION_KEY] = "Chat"
+if update_company_page:  # Mantém apenas a opção para Atualize Empresa
     st.session_state[PAGE_SELECTION_KEY] = "Atualize Empresa"
 
 # Bloco "Adicione Empresa" (completamente comentado) - Já estava comentado
@@ -66,7 +66,7 @@ if st.session_state[PAGE_SELECTION_KEY] == "Atualize Empresa":
     }
 
     print("JORRRRGEEEE")
-    option = st.radio("Escolha entre passar um documento ou uma lista de URLs", ("Documento"))
+    option = st.radio("Escolha entre passar um documento ou uma lista de URLs", ("Documento",)) # Limita as opções
 
     if option == "Documento":
         document_type = st.selectbox("Tipo de Documento", ["csv", "pdf", "txt", "doc", "docx"])
@@ -104,202 +104,173 @@ if st.session_state[PAGE_SELECTION_KEY] == "Atualize Empresa":
                     st.error(f"Erro ao obter URL assinada: {url_assinada_resposta.text}")
 
 
-    # elif option == "Links":  # Início do bloco comentado
-    #     st.subheader("Lista de URLs")
-    #     urls = st.text_input("Novo item")
+    # Bloco "Links" (completamente comentado) - Já estava comentado
+    # elif option == "Links":
+    #     ...
 
-    #     if "lista" not in st.session_state:
-    #         st.session_state["lista"] = []
+# Bloco "Empresas" (completamente comentado)
+# if st.session_state[PAGE_SELECTION_KEY] == "Empresas":
+#     st.title("🏢 Empresas")
 
-    #     if st.button('Adicionar'):
-    #         if urls:
-    #             st.session_state.lista.append(urls)
+#     try:
+#         response = requests.get(ENDPOINT_URL + "/company", headers=HEADERS)
+#         if response.status_code == 200:
+#             data = response.json()
+#         else:
+#             st.error("Falha ao trazer empresas. Por favor, tente novamente.")
+#     except Exception as e:
+#         st.error(f"Um erro ocorreu: {str(e)}")
 
-    #     if st.session_state.lista:
-    #         lista_str = "\n".join(st.session_state.lista)
-    #         st.text_area("Itens da Lista", value=lista_str, height=200)
+#     if 'data' in locals():
+#         for i, company in enumerate(data):
+#             st.text_input(label='Company Name', value=company['name'], disabled=True, key=f"checkbox_{company['name']}-{i}")
 
-    #     update_company_button = st.button("Atualizar Empresa", key="update_company")
+#             company_id = company['id']
 
-    #     if update_company_button and st.session_state.lista:
-    #         prompt = st.text_input("Prompt", "") #Mantive, mas comentado.
-    #         api_data = {
-    #             "company_id": selected_company_id,
-    #             "prompt": prompt,
-    #             "urls": st.session_state.lista
-    #         }
+#             # Verificar se o campo 'sync' existe e é False
+#             if 'sync' in company and company['sync'] == False:
+#                 st.warning("Esta empresa ainda não foi sincronizada.")
 
-    #         try:
-    #             response = requests.post(ENDPOINT_URL + "/company", json=api_data, headers=HEADERS)
-    #             if response.status_code == 200:
-    #                 st.success(f"Empresa '{selected_company_name}' atualizada com sucesso!")
-    #             else:
-    #                 st.error("Falha ao atualizar a empresa. Por favor, tente novamente.")
-    #         except Exception as e:
-    #             st.error(f"Um erro ocorreu: {str(e)}")
-      # Fim do bloco comentado
+#             # try:
+#             #     response = requests.get(ENDPOINT_URL + f"/company/{company_id}")
+#             #     if response.status_code == 200:
+#             #         items = response.json()
+#             #     else:
+#             #         st.error("Falha ao trazer arquivos. Por favor, tente novamente.")
+#             # except Exception as e:
+#             #     st.error(f"Um erro ocorreu: {str(e)}")
 
+#             items = company['archives']
 
-if st.session_state[PAGE_SELECTION_KEY] == "Empresas":
-    st.title("🏢 Empresas")
+#             if items is not None and items:
+#                 for i, archive in enumerate(company['archives']):
+#                     checkbox_selected = st.checkbox(f"🔗 {archive['archive_name']}", key=f"checkbox_{company_id}_{archive['archive_name']}-{i}")
+#                     if checkbox_selected:
+#                         selected_archives.append(archive['archive_name'])
 
-    try:
-        response = requests.get(ENDPOINT_URL + "/company", headers=HEADERS)
-        if response.status_code == 200:
-            data = response.json()
-        else:
-            st.error("Falha ao trazer empresas. Por favor, tente novamente.")
-    except Exception as e:
-        st.error(f"Um erro ocorreu: {str(e)}")
+#                 delete_path = st.button("Excluir", key=f"delete_{company_id}_{i}")
 
-    if 'data' in locals():
-        for i, company in enumerate(data):
-            st.text_input(label='Company Name', value=company['name'], disabled=True, key=f"checkbox_{company['name']}-{i}")
-
-            company_id = company['id']
-
-            # Verificar se o campo 'sync' existe e é False
-            if 'sync' in company and company['sync'] == False:
-                st.warning("Esta empresa ainda não foi sincronizada.")
-
-            # try:
-            #     response = requests.get(ENDPOINT_URL + f"/company/{company_id}")
-            #     if response.status_code == 200:
-            #         items = response.json()
-            #     else:
-            #         st.error("Falha ao trazer arquivos. Por favor, tente novamente.")
-            # except Exception as e:
-            #     st.error(f"Um erro ocorreu: {str(e)}")
-
-            items = company['archives']
-
-            if items is not None and items:
-                for i, archive in enumerate(company['archives']):
-                    checkbox_selected = st.checkbox(f"🔗 {archive['archive_name']}", key=f"checkbox_{company_id}_{archive['archive_name']}-{i}")
-                    if checkbox_selected:
-                        selected_archives.append(archive['archive_name'])
-
-                delete_path = st.button("Excluir", key=f"delete_{company_id}_{i}")
-
-                if delete_path:
-                    body = {
-                        "paths": selected_archives
-                    }
+#                 if delete_path:
+#                     body = {
+#                         "paths": selected_archives
+#                     }
                     
-                    try:
-                        response = requests.post(ENDPOINT_URL + f"/company/{company_id}/delete", json=body, headers=HEADERS)
-                        if response.status_code == 200:
-                            st.success(f"Arquivo excluído com sucesso!")
-                        else:
-                            st.error("Falha ao excluir os arquivos. Por favor, tente novamente.")
-                    except Exception as e:
-                        st.error(f"Um erro ocorreu: {str(e)}")
-if st.session_state[PAGE_SELECTION_KEY] == "Chat":
-    st.title("💬 Chatbot")
+#                     try:
+#                         response = requests.post(ENDPOINT_URL + f"/company/{company_id}/delete", json=body, headers=HEADERS)
+#                         if response.status_code == 200:
+#                             st.success(f"Arquivo excluído com sucesso!")
+#                         else:
+#                             st.error("Falha ao excluir os arquivos. Por favor, tente novamente.")
+#                     except Exception as e:
+#                         st.error(f"Um erro ocorreu: {str(e)}")
 
-    # Obter empresas
-    try:
-        response = requests.get(ENDPOINT_URL + "/company", headers=HEADERS)
-        if response.status_code == 200:
-            companies = response.json()
-            company_id_options = [company["name"] for company in companies]
-        else:
-            st.error("Falha ao obter a lista de empresas. Por favor, tente novamente.")
-            st.stop()
-    except Exception as e:
-        st.error(f"Um erro ocorreu: {str(e)}")
-        st.stop()
+# Bloco "Chat" (completamente comentado)
+# if st.session_state[PAGE_SELECTION_KEY] == "Chat":
+#     st.title("💬 Chatbot")
 
-    # Seleção de empresa
-    selected_company_name = st.selectbox("Empresa", company_id_options)
-    selected_company_id = next(
-        (company['id'] for company in companies if company["name"] == selected_company_name),
-        None,
-    )
+#     # Obter empresas
+#     try:
+#         response = requests.get(ENDPOINT_URL + "/company", headers=HEADERS)
+#         if response.status_code == 200:
+#             companies = response.json()
+#             company_id_options = [company["name"] for company in companies]
+#         else:
+#             st.error("Falha ao obter a lista de empresas. Por favor, tente novamente.")
+#             st.stop()
+#     except Exception as e:
+#         st.error(f"Um erro ocorreu: {str(e)}")
+#         st.stop()
 
-    # Seleção de documento
-    selected_document_name = None
-    if selected_company_id:
-        try:
-            selected_company = next((company for company in companies if company["id"] == selected_company_id), None)
-            if selected_company and "archives" in selected_company:
-                documents = selected_company["archives"]
-                document_options = ["Todos os Documentos"] + [doc["archive_name"] for doc in documents]
-            else:
-                st.error("Nenhum documento encontrado para esta empresa.")
-                st.stop()
-        except Exception as e:
-            st.error(f"Erro ao carregar os arquivos: {str(e)}")
-            st.stop()
+#     # Seleção de empresa
+#     selected_company_name = st.selectbox("Empresa", company_id_options)
+#     selected_company_id = next(
+#         (company['id'] for company in companies if company["name"] == selected_company_name),
+#         None,
+#     )
 
-    # Dropdown para selecionar documento ou "Todos os Documentos"
-    selected_document_name = st.selectbox("Documento", document_options)
-    if selected_document_name == "Todos os Documentos":
-        selected_document_name = None  # Define como None para busca global
+#     # Seleção de documento
+#     selected_document_name = None
+#     if selected_company_id:
+#         try:
+#             selected_company = next((company for company in companies if company["id"] == selected_company_id), None)
+#             if selected_company and "archives" in selected_company:
+#                 documents = selected_company["archives"]
+#                 document_options = ["Todos os Documentos"] + [doc["archive_name"] for doc in documents]
+#             else:
+#                 st.error("Nenhum documento encontrado para esta empresa.")
+#                 st.stop()
+#         except Exception as e:
+#             st.error(f"Erro ao carregar os arquivos: {str(e)}")
+#             st.stop()
 
-    models = {
-        "claude-3-5-sonnet-v2": "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
-    }
+#     # Dropdown para selecionar documento ou "Todos os Documentos"
+#     selected_document_name = st.selectbox("Documento", document_options)
+#     if selected_document_name == "Todos os Documentos":
+#         selected_document_name = None  # Define como None para busca global
 
-    model_id_options = list(models.keys())
-    selected_model_id = st.selectbox("Modelo", model_id_options)
-    show_citations = st.checkbox("Mostrar citações")  # Checkbox movido para antes do loop de mensagens
+#     models = {
+#         "claude-3-5-sonnet-v2": "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+#     }
 
-    chosen_company_id = selected_company_id
-    chosen_model_id = models[selected_model_id]
+#     model_id_options = list(models.keys())
+#     selected_model_id = st.selectbox("Modelo", model_id_options)
+#     show_citations = st.checkbox("Mostrar citações")  # Checkbox movido para antes do loop de mensagens
 
-    # Mensagens do chat
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [
-            {"role": "assistant", "content": "Como posso ajudar você?"}
-        ]
+#     chosen_company_id = selected_company_id
+#     chosen_model_id = models[selected_model_id]
 
-    for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
+#     # Mensagens do chat
+#     if "messages" not in st.session_state:
+#         st.session_state["messages"] = [
+#             {"role": "assistant", "content": "Como posso ajudar você?"}
+#         ]
 
-    # Barra de entrada de texto aparece apenas na aba "Chat"
-    if prompt := st.chat_input():
-        # Adicionar a mensagem do usuário ao estado
-        user_message = {"role": "user", "content": prompt}
-        st.session_state.messages.append(user_message)
+#     for msg in st.session_state.messages:
+#         st.chat_message(msg["role"]).write(msg["content"])
 
-        # Renderizar a mensagem do usuário imediatamente
-        st.chat_message("user").write(prompt)
+#     # Barra de entrada de texto aparece apenas na aba "Chat"
+#     if prompt := st.chat_input():
+#         # Adicionar a mensagem do usuário ao estado
+#         user_message = {"role": "user", "content": prompt}
+#         st.session_state.messages.append(user_message)
 
-        # Construção do payload condicional
-        request_data = {
-            "question": prompt,
-            "companyId": chosen_company_id,
-            "modelId": chosen_model_id,
-        }
-        if selected_document_name:
-            request_data["document_id"] = selected_document_name
+#         # Renderizar a mensagem do usuário imediatamente
+#         st.chat_message("user").write(prompt)
 
-        # Enviar requisição para a API
-        try:
-            response = requests.post(ENDPOINT_URL + "/chat", json=request_data, headers=HEADERS)
-            if response.status_code == 200:
-                response_data = response.json()
-                response_text = response_data["response"]
+#         # Construção do payload condicional
+#         request_data = {
+#             "question": prompt,
+#             "companyId": chosen_company_id,
+#             "modelId": chosen_model_id,
+#         }
+#         if selected_document_name:
+#             request_data["document_id"] = selected_document_name
 
-                # Adicionar a resposta ao estado
-                assistant_message = {"role": "assistant", "content": response_text}
-                st.session_state.messages.append(assistant_message)
+#         # Enviar requisição para a API
+#         try:
+#             response = requests.post(ENDPOINT_URL + "/chat", json=request_data, headers=HEADERS)
+#             if response.status_code == 200:
+#                 response_data = response.json()
+#                 response_text = response_data["response"]
 
-                # Renderizar a resposta do assistente imediatamente
-                st.chat_message("assistant").write(response_text)
+#                 # Adicionar a resposta ao estado
+#                 assistant_message = {"role": "assistant", "content": response_text}
+#                 st.session_state.messages.append(assistant_message)
 
-                if show_citations and "citation" in response_data:
-                    for citation_block in response_data["citation"]:
-                        citation_text = citation_block["content"]["text"]
-                        document_uri = citation_block["location"]["s3Location"]["uri"]
-                        document_name = citation_block["metadata"]["name"]
+#                 # Renderizar a resposta do assistente imediatamente
+#                 st.chat_message("assistant").write(response_text)
 
-                        st.markdown(f"**Citação do documento:** {document_name}")
-                        st.markdown(f"**URI do Documento:** {document_uri}")
-                        st.markdown(f"**Conteúdo:**\n{citation_text}")
-                        st.markdown("---")
-            else:
-                st.error("Falhou em obter uma resposta da API.")
-        except Exception as e:
-            st.error(f"Ocorreu um erro ao tentar enviar o pedido.")
+#                 if show_citations and "citation" in response_data:
+#                     for citation_block in response_data["citation"]:
+#                         citation_text = citation_block["content"]["text"]
+#                         document_uri = citation_block["location"]["s3Location"]["uri"]
+#                         document_name = citation_block["metadata"]["name"]
+
+#                         st.markdown(f"**Citação do documento:** {document_name}")
+#                         st.markdown(f"**URI do Documento:** {document_uri}")
+#                         st.markdown(f"**Conteúdo:**\n{citation_text}")
+#                         st.markdown("---")
+#             else:
+#                 st.error("Falhou em obter uma resposta da API.")
+#         except Exception as e:
+#             st.error(f"Ocorreu um erro ao tentar enviar o pedido.")
